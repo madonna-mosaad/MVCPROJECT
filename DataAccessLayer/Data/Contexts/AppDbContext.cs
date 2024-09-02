ef19d18692edeb57c8f4a4062db360dddc70c263
madonna-mosaad <madonnamosaad86@gmail.com>
2024-09-02 20:10:57 +0300

    DataAccessLayer01

---
+++ DataAccessLayer/Data/Contexts/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data.Contexts
+{
+    internal class AppDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Servsr=DESKTOP-2AMJ5HN ; Database=MVC ; Trusted_Connection=True ; Encrypt= False ; MultipleActiveResultSets=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ DataAccessLayer/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    //poco class (has properties only that present columns)
+    internal class Department
+    {
+        //PK to use in DB
+        public int Id { get; set; }
+        //to use in view
+        public int Code {  get; set; }
+        public string Name { get; set; }
+        public DateTime Creation { get; set; }
+    }
+}
